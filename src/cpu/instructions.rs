325dcade545f1b7e1ec4db32cdad9411d4266536
Raphaël Gilles <rgilles@student.42.fr>
2023-04-02 18:26:50 +0200

    Added INCss, DECss, ADDHLss and ADDSPe instructions

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -8,14 +8,28 @@ pub enum ArithmeticOperand{
 	RegA, RegB, RegC, RegD, RegE, RegH, RegL, HLPointee, RawByte
 }
 #[derive(Debug, Clone, Copy)]
-pub enum IncDecTarget{
+pub enum LargeArithmeticOperand{
+	RegsBC, RegsDE, RegsHL, RegSP
+}
+#[derive(Debug, Clone, Copy)]
+pub enum TargetReg{
 	RegA, RegB, RegC, RegD, RegE, RegH, RegL, HLPointee
+}
+#[derive(Debug, Clone, Copy)]
+pub enum TargetRegPair{
+	RegsBC, RegsDE, RegsHL, RegSP
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum Instruction {
 	NOP(InstrLength, InstrCycles),
+	INCs(InstrLength, InstrCycles, TargetReg),
+	INCss(InstrLength, InstrCycles, TargetRegPair),
+	DECs(InstrLength, InstrCycles, TargetReg),
+	DECss(InstrLength, InstrCycles, TargetRegPair),
 	ADDAs(InstrLength, InstrCycles, ArithmeticOperand),
+	ADDHLss(InstrLength, InstrCycles, LargeArithmeticOperand),
+	ADDSPe(InstrLength, InstrCycles),
 	ADCAs(InstrLength, InstrCycles, ArithmeticOperand),
 	SUBs(InstrLength, InstrCycles, ArithmeticOperand),
 	SBCAs(InstrLength, InstrCycles, ArithmeticOperand),
@@ -23,30 +37,40 @@ pub enum Instruction {
 	XORs(InstrLength, InstrCycles, ArithmeticOperand),
 	ORs(InstrLength, InstrCycles, ArithmeticOperand),
 	CPs(InstrLength, InstrCycles, ArithmeticOperand),
-	INCs(InstrLength, InstrCycles, IncDecTarget),
-	DECs(InstrLength, InstrCycles, IncDecTarget),
 }
 
 impl Instruction {
 	pub fn from_opcode(opcode: u8) -> Option<Instruction> {
 		match opcode {
 			0x00 => Some(Instruction::NOP(1, 4)),
-			0x04 => Some(Instruction::INCs(1, 4, IncDecTarget::RegB)),
-			0x05 => Some(Instruction::DECs(1, 4, IncDecTarget::RegB)),
-			0x0C => Some(Instruction::INCs(1, 4, IncDecTarget::RegC)),
-			0x0D => Some(Instruction::DECs(1, 4, IncDecTarget::RegC)),
-			0x14 => Some(Instruction::INCs(1, 4, IncDecTarget::RegD)),
-			0x15 => Some(Instruction::DECs(1, 4, IncDecTarget::RegD)),
-			0x1C => Some(Instruction::INCs(1, 4, IncDecTarget::RegE)),
-			0x1D => Some(Instruction::DECs(1, 4, IncDecTarget::RegE)),
-			0x24 => Some(Instruction::INCs(1, 4, IncDecTarget::RegH)),
-			0x25 => Some(Instruction::DECs(1, 4, IncDecTarget::RegH)),
-			0x2C => Some(Instruction::INCs(1, 4, IncDecTarget::RegL)),
-			0x2D => Some(Instruction::DECs(1, 4, IncDecTarget::RegL)),
-			0x34 => Some(Instruction::INCs(1, 12, IncDecTarget::HLPointee)),
-			0x35 => Some(Instruction::DECs(1, 12, IncDecTarget::HLPointee)),
-			0x3C => Some(Instruction::INCs(1, 4, IncDecTarget::RegA)),
-			0x3D => Some(Instruction::DECs(1, 4, IncDecTarget::RegA)),
+			0x03 => Some(Instruction::INCss(1, 8, TargetRegPair::RegsBC)),
+			0x04 => Some(Instruction::INCs(1, 4, TargetReg::RegB)),
+			0x05 => Some(Instruction::DECs(1, 4, TargetReg::RegB)),
+			0x09 => Some(Instruction::ADDHLss(1, 8, LargeArithmeticOperand::RegsBC)),
+			0x0B => Some(Instruction::DECss(1, 8, TargetRegPair::RegsBC)),
+			0x0C => Some(Instruction::INCs(1, 4, TargetReg::RegC)),
+			0x0D => Some(Instruction::DECs(1, 4, TargetReg::RegC)),
+			0x13 => Some(Instruction::INCss(1, 8, TargetRegPair::RegsDE)),
+			0x14 => Some(Instruction::INCs(1, 4, TargetReg::RegD)),
+			0x15 => Some(Instruction::DECs(1, 4, TargetReg::RegD)),
+			0x19 => Some(Instruction::ADDHLss(1, 8, LargeArithmeticOperand::RegsDE)),
+			0x1B => Some(Instruction::DECss(1, 8, TargetRegPair::RegsDE)),
+			0x1C => Some(Instruction::INCs(1, 4, TargetReg::RegE)),
+			0x1D => Some(Instruction::DECs(1, 4, TargetReg::RegE)),
+			0x23 => Some(Instruction::INCss(1, 8, TargetRegPair::RegsHL)),
+			0x24 => Some(Instruction::INCs(1, 4, TargetReg::RegH)),
+			0x25 => Some(Instruction::DECs(1, 4, TargetReg::RegH)),
+			0x29 => Some(Instruction::ADDHLss(1, 8, LargeArithmeticOperand::RegsHL)),
+			0x2B => Some(Instruction::DECss(1, 8, TargetRegPair::RegsHL)),
+			0x2C => Some(Instruction::INCs(1, 4, TargetReg::RegL)),
+			0x2D => Some(Instruction::DECs(1, 4, TargetReg::RegL)),
+			0x33 => Some(Instruction::INCss(1, 8, TargetRegPair::RegSP)),
+			0x34 => Some(Instruction::INCs(1, 12, TargetReg::HLPointee)),
+			0x35 => Some(Instruction::DECs(1, 12, TargetReg::HLPointee)),
+			0x39 => Some(Instruction::ADDHLss(1, 8, LargeArithmeticOperand::RegSP)),
+			0x3B => Some(Instruction::DECss(1, 8, TargetRegPair::RegSP)),
+			0x3C => Some(Instruction::INCs(1, 4, TargetReg::RegA)),
+			0x3D => Some(Instruction::DECs(1, 4, TargetReg::RegA)),
 
 			0x80 => Some(Instruction::ADDAs(1, 4, ArithmeticOperand::RegB)),
 			0x81 => Some(Instruction::ADDAs(1, 4, ArithmeticOperand::RegC)),
@@ -118,6 +142,7 @@ impl Instruction {
 			0xD6 => Some(Instruction::SUBs(2, 8, ArithmeticOperand::RawByte)),
 			0xDE => Some(Instruction::SBCAs(2, 8, ArithmeticOperand::RawByte)),
 			0xE6 => Some(Instruction::ANDs(2, 8, ArithmeticOperand::RawByte)),
+			0xE8 => Some(Instruction::ADDSPe(2, 16)),
 			0xEE => Some(Instruction::XORs(2, 8, ArithmeticOperand::RawByte)),
 			0xF6 => Some(Instruction::ORs(2, 8, ArithmeticOperand::RawByte)),
 			0xFE => Some(Instruction::CPs(2, 8, ArithmeticOperand::RawByte)),
@@ -127,7 +152,7 @@ impl Instruction {
 }
 
 impl Cpu {
-	pub fn get_arith_operand_value(&mut self, operand: ArithmeticOperand) -> u8 {
+	pub fn get_operand_value(&mut self, operand: ArithmeticOperand) -> u8 {
 		match operand {
 			ArithmeticOperand::RegA => self.registers.a,
 			ArithmeticOperand::RegB => self.registers.b,
@@ -140,28 +165,52 @@ impl Cpu {
 			ArithmeticOperand::RawByte => self.fetch_pc()
 		}
 	}
-	pub fn get_incdec_target_value(&self, target: IncDecTarget) -> u8 {
-		match target {
-			IncDecTarget::RegA => self.registers.a,
-			IncDecTarget::RegB => self.registers.b,
-			IncDecTarget::RegC => self.registers.c,
-			IncDecTarget::RegD => self.registers.d,
-			IncDecTarget::RegE => self.registers.e,
-			IncDecTarget::RegH => self.registers.h,
-			IncDecTarget::RegL => self.registers.l,
-			IncDecTarget::HLPointee => self.registers.get_hl_pointee(&self.memory_bus),
+	pub fn get_large_operand_value(&mut self, operand: LargeArithmeticOperand) -> u16 {
+		match operand {
+			LargeArithmeticOperand::RegsBC => self.registers.get_bc(),
+			LargeArithmeticOperand::RegsDE => self.registers.get_de(),
+			LargeArithmeticOperand::RegsHL => self.registers.get_hl(),
+			LargeArithmeticOperand::RegSP => self.registers.stack_pointer,
 		}
 	}
-	pub fn set_incdec_target_value(&mut self, target: IncDecTarget, data: u8) {
+	pub fn get_target_reg_value(&self, target: TargetReg) -> u8 {
 		match target {
-			IncDecTarget::RegA => {self.registers.a = data}
-			IncDecTarget::RegB => {self.registers.b = data}
-			IncDecTarget::RegC => {self.registers.c = data}
-			IncDecTarget::RegD => {self.registers.d = data}
-			IncDecTarget::RegE => {self.registers.e = data}
-			IncDecTarget::RegH => {self.registers.h = data}
-			IncDecTarget::RegL => {self.registers.l = data}
-			IncDecTarget::HLPointee => {self.registers.set_hl_pointee(&mut self.memory_bus, data)}
+			TargetReg::RegA => self.registers.a,
+			TargetReg::RegB => self.registers.b,
+			TargetReg::RegC => self.registers.c,
+			TargetReg::RegD => self.registers.d,
+			TargetReg::RegE => self.registers.e,
+			TargetReg::RegH => self.registers.h,
+			TargetReg::RegL => self.registers.l,
+			TargetReg::HLPointee => self.registers.get_hl_pointee(&self.memory_bus),
+		}
+	}
+	pub fn set_target_reg_value(&mut self, target: TargetReg, data: u8) {
+		match target {
+			TargetReg::RegA => {self.registers.a = data}
+			TargetReg::RegB => {self.registers.b = data}
+			TargetReg::RegC => {self.registers.c = data}
+			TargetReg::RegD => {self.registers.d = data}
+			TargetReg::RegE => {self.registers.e = data}
+			TargetReg::RegH => {self.registers.h = data}
+			TargetReg::RegL => {self.registers.l = data}
+			TargetReg::HLPointee => {self.registers.set_hl_pointee(&mut self.memory_bus, data)}
+		}
+	}
+	pub fn set_target_pair_value(&mut self, operand: TargetRegPair, data: u16) {
+		match operand {
+			TargetRegPair::RegsBC => {self.registers.set_bc(data)}
+			TargetRegPair::RegsDE => {self.registers.set_de(data)}
+			TargetRegPair::RegsHL => {self.registers.set_hl(data)}
+			TargetRegPair::RegSP => {self.registers.stack_pointer = data}
+		}
+	}
+	pub fn get_target_pair_value(&self, operand: TargetRegPair) -> u16 {
+		match operand {
+			TargetRegPair::RegsBC => self.registers.get_bc(),
+			TargetRegPair::RegsDE => self.registers.get_de(),
+			TargetRegPair::RegsHL => self.registers.get_hl(),
+			TargetRegPair::RegSP => self.registers.stack_pointer
 		}
 	}
 	pub fn execute_op(&mut self, instruction: Instruction) {
@@ -169,7 +218,7 @@ impl Cpu {
 			Instruction::NOP(_, _) => {}
 			Instruction::ADDAs(_, _, _operand) | Instruction::ADCAs(_, _, _operand) => {
 				let reg_a_content = self.registers.a as u16;
-				let operand_val = self.get_arith_operand_value(_operand) as u16;
+				let operand_val = self.get_operand_value(_operand) as u16;
 				// if instr is ADC and carry flag is true, let carry = 1;
 				let carry_val = if let Instruction::ADCAs(_, _, _) = instruction {self.registers.f.carry as u16} else {0};
 				let r = reg_a_content + operand_val + carry_val;
@@ -181,7 +230,7 @@ impl Cpu {
 			}
 			Instruction::SUBs(_, _, _operand) | Instruction::SBCAs(_, _, _operand) => {
 				let reg_a_content = self.registers.a as u16;
-				let operand_val = self.get_arith_operand_value(_operand) as u16;
+				let operand_val = self.get_operand_value(_operand) as u16;
 				// if instr is ADC and carry flag is true, let carry = 1;
 				let carry_val = if let Instruction::SBCAs(_, _, _) = instruction {self.registers.f.carry as u16} else {0};
 				let r = reg_a_content - operand_val - carry_val;
@@ -192,7 +241,7 @@ impl Cpu {
 				self.registers.a = r as u8;
 			}
 			Instruction::ANDs(_, _, _operand) => {
-				let operand_val = self.get_arith_operand_value(_operand);
+				let operand_val = self.get_operand_value(_operand);
 				self.registers.a &= operand_val;
 				self.registers.f.zero = self.registers.a == 0;
 				self.registers.f.substract = false;
@@ -200,7 +249,7 @@ impl Cpu {
 				self.registers.f.carry = false;
 			}
 			Instruction::XORs(_, _, _operand) => {
-				let operand_val = self.get_arith_operand_value(_operand);
+				let operand_val = self.get_operand_value(_operand);
 				self.registers.a ^= operand_val;
 				self.registers.f.zero = self.registers.a == 0;
 				self.registers.f.substract = false;
@@ -208,7 +257,7 @@ impl Cpu {
 				self.registers.f.carry = false;
 			}
 			Instruction::ORs(_, _, _operand) => {
-				let operand_val = self.get_arith_operand_value(_operand);
+				let operand_val = self.get_operand_value(_operand);
 				self.registers.a |= operand_val;
 				self.registers.f.zero = self.registers.a == 0;
 				self.registers.f.substract = false;
@@ -217,7 +266,7 @@ impl Cpu {
 			}
 			Instruction::CPs(_, _, _operand) => {
 				let reg_a_content = self.registers.a as u16;
-				let operand_val = self.get_arith_operand_value(_operand) as u16;
+				let operand_val = self.get_operand_value(_operand) as u16;
 				let r = reg_a_content - operand_val;
 				self.registers.f.zero = r == 0;
 				self.registers.f.substract = true;
@@ -225,18 +274,46 @@ impl Cpu {
 				self.registers.f.carry = r & 0x100 != 0;
 			}
 			Instruction::INCs(_, _, _target) => {
-				let target_value = self.get_incdec_target_value(_target);
+				let target_value = self.get_target_reg_value(_target);
 				self.registers.f.zero = target_value == 0xFF;
 				self.registers.f.substract = false;
 				self.registers.f.half_carry = (target_value & 0xF) + 1 >= 0x10;
-				self.set_incdec_target_value(_target, target_value + 1);
+				self.set_target_reg_value(_target, target_value + 1);
 			}
 			Instruction::DECs(_, _, _target) => {
-				let target_value = self.get_incdec_target_value(_target);
+				let target_value = self.get_target_reg_value(_target);
 				self.registers.f.zero = target_value == 0x01;
 				self.registers.f.substract = true;
 				self.registers.f.half_carry = target_value & 0xF < 1;
-				self.set_incdec_target_value(_target, target_value - 1);
+				self.set_target_reg_value(_target, target_value - 1);
+			}
+			Instruction::INCss(_, _, _target) => {
+				self.set_target_pair_value(_target, self.get_target_pair_value(_target) + 1);
+			}
+			Instruction::DECss(_, _, _target) => {
+				self.set_target_pair_value(_target, self.get_target_pair_value(_target) - 1);
+			}
+			Instruction::ADDHLss(_, _, _operand) => {
+				let hl_value = self.registers.get_hl();
+				let operand_value = self.get_large_operand_value(_operand);
+				let r = hl_value + operand_value;
+				self.registers.f.substract = false;
+				self.registers.f.half_carry = (hl_value & 0x0FFF) + (operand_value & 0x0FFF) >= 0x1000;
+				self.registers.f.carry = r < hl_value;
+				self.registers.set_hl(r);
+			}
+			Instruction::ADDSPe(_, _) => {
+				let sp_value = self.registers.stack_pointer;
+				let operand_value = self.fetch_pc() as i8;
+
+				let b = (operand_value as u8) as u16;
+				let n = (sp_value as i32 + b as i32) as u16;
+
+				self.registers.f.zero = false;
+				self.registers.f.substract = false;
+				self.registers.f.half_carry = sp_value & 0x000F + b & 0x000F >= 0x10;
+				self.registers.f.carry = sp_value & 0x00FF + b >= 0x100;
+				self.registers.stack_pointer = n;
 			}
 		}
 	}
--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -66,12 +66,12 @@ impl Registers {
 	// pub fn get_af(&self) -> u16 {
 	// 	(self.a as u16) << 8 | u8::from(self.f) as u16
 	// }
-	// pub fn get_bc(&self) -> u16 {
-	// 	(self.b as u16) << 8 | self.c as u16
-	// }
-	// pub fn get_de(&self) -> u16 {
-	// 	(self.d as u16) << 8 | self.e as u16
-	// }
+	pub fn get_bc(&self) -> u16 {
+		(self.b as u16) << 8 | self.c as u16
+	}
+	pub fn get_de(&self) -> u16 {
+		(self.d as u16) << 8 | self.e as u16
+	}
 	pub fn get_hl(&self) -> u16 {
 		(self.h as u16) << 8 | self.l as u16
 	}
