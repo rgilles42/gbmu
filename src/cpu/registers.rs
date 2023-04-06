8c6b660658e31e0a968a8c86a66c605a21699d3e
Raphaël Gilles <rgilles@student.42.fr>
2023-04-06 18:01:54 +0200

    Fixed endianness of register pairs accessors

--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -51,67 +51,76 @@ pub struct Registers {
 	pub stack_pointer: u16,
 }
 
+/* Though the GB MCU is an 8-bit, little-endian CPU, the register pairs are assembled and expressed as big-endian... */
+
 impl Registers {
 	pub fn new() -> Self {
 		Registers {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0.into(), h: 0, l: 0, program_counter: 0x0000, stack_pointer : 0x0000}
 	}
 	pub fn init(&mut self) {
 		self.program_counter = 0x100;
-		self.set_af_little_endian(0x01B0);			// GB/SGB - 0x01B0; GBP - 0xFFB0; GBC - 0x11B0
-		self.set_bc_little_endian(0x0013);
-		self.set_de_little_endian(0x00D8);
-		self.set_hl_little_endian(0x014D);
+		self.set_af_big_endian(0x01B0);			// GB/SGB - 0x01B0; GBP - 0xFFB0; GBC - 0x11B0
+		self.set_bc_big_endian(0x0013);
+		self.set_de_big_endian(0x00D8);
+		self.set_hl_big_endian(0x014D);
 		self.stack_pointer = 0xFFFE;
 	}
-	// pub fn get_af_little_endian(&self) -> u16 {
-	// 	(self.a as u16) << 8 | u8::from(self.f) as u16
-	// }
-	// pub fn get_bc_little_endian(&self) -> u16 {
-	// 	(self.b as u16) << 8 | self.c as u16
-	// }
-	// pub fn get_de_little_endian(&self) -> u16 {
-	// 	(self.d as u16) << 8 | self.e as u16
-	// }
-	// pub fn get_hl_little_endian(&self) -> u16 {
-	// 	(self.h as u16) << 8 | self.l as u16
-	// }
+	pub fn get_af_big_endian(&self) -> u16 {
+		(self.a as u16) << 8 | u8::from(self.f) as u16
+	}
 	pub fn get_bc_big_endian(&self) -> u16 {
-		(self.c as u16) << 8 | self.b as u16
+		(self.b as u16) << 8 | self.c as u16
 	}
 	pub fn get_de_big_endian(&self) -> u16 {
-		(self.e as u16) << 8 | self.d as u16
+		(self.d as u16) << 8 | self.e as u16
 	}
 	pub fn get_hl_big_endian(&self) -> u16 {
-		(self.l as u16) << 8 | self.h as u16
+		(self.h as u16) << 8 | self.l as u16
 	}
-	pub fn set_af_little_endian(&mut self, little_endian_value: u16) {
-		self.a = (little_endian_value >> 8) as u8;
-		self.f = (little_endian_value as u8).into();
-	}
-	pub fn set_bc_little_endian(&mut self, little_endian_value: u16) {
-		self.b = (little_endian_value >> 8) as u8;
-		self.c = little_endian_value as u8;
-	}
-	pub fn set_de_little_endian(&mut self, little_endian_value: u16) {
-		self.d = (little_endian_value >> 8) as u8;
-		self.e = little_endian_value as u8;
-	}
-	pub fn set_hl_little_endian(&mut self, little_endian_value: u16) {
-		self.h = (little_endian_value >> 8) as u8;
-		self.l = little_endian_value as u8;
+	pub fn set_af_big_endian(&mut self, big_endian_value: u16) {
+		self.a = (big_endian_value >> 8) as u8;
+		self.f = (big_endian_value as u8).into();
 	}
 	pub fn set_bc_big_endian(&mut self, big_endian_value: u16) {
-		self.b = big_endian_value as u8;
-		self.c = (big_endian_value >> 8) as u8;
+		self.b = (big_endian_value >> 8) as u8;
+		self.c = big_endian_value as u8;
 	}
 	pub fn set_de_big_endian(&mut self, big_endian_value: u16) {
-		self.d = big_endian_value as u8;
-		self.e = (big_endian_value >> 8) as u8;
+		self.d = (big_endian_value >> 8) as u8;
+		self.e = big_endian_value as u8;
 	}
 	pub fn set_hl_big_endian(&mut self, big_endian_value: u16) {
-		self.h = big_endian_value as u8;
-		self.l = (big_endian_value >> 8) as u8;
+		self.h = (big_endian_value >> 8) as u8;
+		self.l = big_endian_value as u8;
 	}
+	// pub fn get_af_little_endian(&self) -> u16 {
+	// 	(u8::from(self.f) as u16) << 8 | self.a as u16
+	// }
+	// pub fn get_bc_little_endian(&self) -> u16 {
+	// 	(self.c as u16) << 8 | self.b as u16
+	// }
+	// pub fn get_de_little_endian(&self) -> u16 {
+	// 	(self.e as u16) << 8 | self.d as u16
+	// }
+	// pub fn get_hl_little_endian(&self) -> u16 {
+	// 	(self.l as u16) << 8 | self.h as u16
+	// }
+	// pub fn set_af_little_endian(&mut self, little_endian_value: u16) {
+	// 	self.a = little_endian_value as u8;
+	// 	self.f = ((little_endian_value >> 8) as u8).into();
+	// }
+	// pub fn set_bc_little_endian(&mut self, little_endian_value: u16) {
+	// 	self.b = little_endian_value as u8;
+	// 	self.c = (little_endian_value >> 8) as u8;
+	// }
+	// pub fn set_de_little_endian(&mut self, little_endian_value: u16) {
+	// 	self.d = little_endian_value as u8;
+	// 	self.e = (little_endian_value >> 8) as u8;
+	// }
+	// pub fn set_hl_little_endian(&mut self, little_endian_value: u16) {
+	// 	self.h = little_endian_value as u8;
+	// 	self.l = (little_endian_value >> 8) as u8;
+	// }
 	pub fn get_bc_pointee(&self, memory_bus: &MemoryBus) -> u8 {
 		memory_bus.read_byte(self.get_bc_big_endian())
 	}
